affad3833d9d2e07712fb240a7d55e251fa5e9a5
djackmoura1-coder <djackmoura1@gmail.com>
2026-07-14 09:52:04 -0300

    Update solicitar.py

--- _pages/solicitar.py
+++ _pages/solicitar.py
@@ -11,6 +11,31 @@ if "logado" not in st.session_state or not st.session_state.logado:
 
 st.title("📌 Solicitar Bloqueio de Pedido")
 
+st.markdown("""
+<style>
+
+/* Todos os botões do Streamlit */
+[data-testid="stButton"] > button {
+    background-color: #16a34a !important;
+    color: white !important;
+    font-weight: 700;
+    border-radius: 8px;
+    border: none;
+    height: 46px;
+}
+
+[data-testid="stButton"] > button:hover {
+    background-color: #15803d !important;
+    color: white !important;
+}
+
+[data-testid="stButton"] > button:active {
+    background-color: #166534 !important;
+}
+
+</style>
+""", unsafe_allow_html=True)
+
 # 🔗 CONEXÃO
 scope = [
     "https://www.googleapis.com/auth/spreadsheets",
